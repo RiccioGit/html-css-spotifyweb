9aca117e23e6e97bccd1cc17cf49b8db5df1404c
Roberto Riccioli <riccioli.rob@gmail.com>
2020-06-11 15:39:18 +0200

    aggiunta Javascript

---
+++ prova.js
@@ -0,0 +1,12 @@
+//  commento di prova monoriga
+
+document.getElementById('nome-utente').innerHTML="Roberto Riccioli";
+
+/*
+Questo invece è un commento di prova su più righe
+Questo invece è un commento di prova su più righe
+Questo invece è un commento di prova su più righe
+Questo invece è un commento di prova su più righe
+Questo invece è un commento di prova su più righe
+
+*/
